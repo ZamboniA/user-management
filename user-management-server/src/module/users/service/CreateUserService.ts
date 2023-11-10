280a44fd32317a45cd1bfec56c9e00d9b0d4dc94
zambonia <andersonzamboni18@gmail.com>
2023-11-10 09:31:21 -0300

    Create controller, routes and AppError && correction migrations

--- user-management-server/src/module/users/service/CreateUserService.ts
+++ user-management-server/src/module/users/service/CreateUserService.ts
@@ -1,15 +1,33 @@
 import { PrismaClient } from '@prisma/client';
+import AppError from '../../../shared/errors/AppError';
 
-interface User{
-    name: string
-    email: string
-    password: string
+
+const prisma = new PrismaClient();
+
+interface User {
+    name: string;
+    email: string;
+    password: string;
 }
 
-export default class CreateUserService{
-
-
-
-    async execute({ name, email, password }: User) {
-
+class CreateUserService {
+    public async execute({ name, email, password }: User) {
+        const userExists = await prisma.user.findFirst({ where: { email } });
+        
+        if (userExists) {
+            throw new AppError('Email adress already used.', 500);
     }
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password,
+        },
+    });
+
+    return user;
+    }
+}
+
+export default CreateUserService;
